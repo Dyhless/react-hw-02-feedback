130a2dfdae1b208ee1cbcd9314b1014da0ed576a
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-13 20:05:46 +0200

    Create FeedbackOptions.styled.jsx

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const ButtonWrapper = styled.div`
+  margin-bottom: 10px;
+`;
+
+export const Button = styled.button`
+  padding: 10px;
+  background-color: #ccc;
+  border: none;
+  border-radius: 4px;
+  margin-right: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  color: #333;
+
+  &:last-child {
+    margin-right: 0;
+  }
+
+  &:hover {
+    background-color: #aaa;
+  }
+`;
