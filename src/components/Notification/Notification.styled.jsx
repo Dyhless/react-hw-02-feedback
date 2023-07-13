95393e0e8d03ee6736f68f5aaff82132972b26d5
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-13 20:05:26 +0200

    Create Notification.styled.jsx

---
+++ src/components/Notification/Notification.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const NotificationWrapper = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const NotificationText = styled.p`
+  font-size: 18px;
+  color: red;
+  text-align: center;
+  font-weight: bold;
+`;
