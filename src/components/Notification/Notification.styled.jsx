16fd381ead7747c9e677a7a10d6257851c496b44
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-13 20:33:42 +0200

    Update Notification.styled.jsx

--- src/components/Notification/Notification.styled.jsx
+++ src/components/Notification/Notification.styled.jsx
@@ -1,12 +1,10 @@
 import styled from 'styled-components';
 
 export const NotificationWrapper = styled.div`
-  margin-bottom: 20px;
+  text-align: center;
 `;
 
 export const NotificationText = styled.p`
   font-size: 18px;
   color: red;
-  text-align: center;
-  font-weight: bold;
 `;
