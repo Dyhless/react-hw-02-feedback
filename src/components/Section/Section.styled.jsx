249de62eccb562ec64813b5d3924a4aa18c62d42
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-13 20:04:42 +0200

    Create Section.styled.jsx

---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SectionWrapper = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const SectionTitle = styled.h2`
+  font-size: 24px;
+  color: #333;
+  text-align: center;
+`;
